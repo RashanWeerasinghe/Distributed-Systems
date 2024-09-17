024d0c0bf27e5c2565ad30e4e9c896546fa9eef7
RashanWeerasinghe <malithrashan03@gmail.com>
2024-09-17 18:50:47 +0530

    remove resttemplate and add webclient

--- Microservices Communication/user-service/src/main/java/com/example/user_service/service/impl/UserServiceImpl.java
+++ Microservices Communication/user-service/src/main/java/com/example/user_service/service/impl/UserServiceImpl.java
@@ -10,13 +10,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
+
+    private WebClient webClient;
 
     @Override
     public User saveUser(User user) {
@@ -30,13 +33,15 @@ public class UserServiceImpl implements UserService {
         User user=userRepository.findById(userId).get();
         UserDto userDto=mapToUser(user);
 
-        ResponseEntity<DepartmentDto>responseEntity=restTemplate
-                .getForEntity("http://localhost:8080/api/departments/" +user.getDepartmentId()
-                ,DepartmentDto.class);
+//        ResponseEntity<DepartmentDto>responseEntity=restTemplate
+//                .getForEntity("http://localhost:8080/api/departments/" +user.getDepartmentId()
+//                ,DepartmentDto.class);
 
-        DepartmentDto departmentDto = responseEntity.getBody();
-
-        System.out.println(responseEntity.getStatusCode());
+        DepartmentDto departmentDto =webClient.get()
+                .uri("http://localhost:8080/api/departments/" + user.getDepartmentId())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         responseDto.setUser(userDto);
         responseDto.setDepartment(departmentDto);
